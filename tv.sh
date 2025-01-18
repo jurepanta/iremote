8791b94964b7165dbcb428cec928022f07473168
jurepanta <jurepanta@gmail.com>
2025-01-18 09:36:47 +0100

    Update tv.sh

--- tv.sh
+++ tv.sh
@@ -3,7 +3,7 @@ while true
 do
 if pgrep -x "VLC" >/dev/null
 then
-num=$(awk '{n+=gsub("0x18", "&")}END{print n}' /Users/jore/ir_received)
+num=$(awk '{n+=gsub("0x18", "&")}END{print n}' /Users/username/ir_received)
 if [[ $num -gt 3 ]]; then
 fullscreen=$(osascript -e 'tell application "VLC"
                 get fullscreen mode
@@ -17,7 +17,7 @@ osascript -e '
                 tell application "System Events" to tell process "VLC"
                 keystroke "+" using {command down}
                 end tell';
-open -a /Applications/VLC.app/Contents/MacOS/VLC /Users/jore/Music/radio.xspf
+open -a /Applications/VLC.app/Contents/MacOS/VLC /Users/username/Music/radio.xspf
 osascript -e 'activate application "System Events"';
 fi
 
